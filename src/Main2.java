47019fd2d9c99eb031b5b87851585afc2966858e
kooreung <crisishyun@gmail.com>
2024-02-19 12:41:07 +0900

    커밋 두번째

---
+++ src/Main2.java
@@ -0,0 +1,5 @@
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("새 파일에서 인사");
+    }
+}
---
+++ src/Main3.java
@@ -0,0 +1,10 @@
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println("메인1");
+        System.out.println("메인2");
+        System.out.println("메인3");
+        System.out.println("메인4");
+        System.out.println("메인5");
+        System.out.println("메인6");
+    }
+}
