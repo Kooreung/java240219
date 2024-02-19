f446c87ea98e2e092f5fe73ed6b6be616c08ed7e
kooreung <crisishyun@gmail.com>
2024-02-19 16:21:24 +0900

    remove .idea

--- src/c02/lecture/p02type/C02Integer.java
+++ src/c02/lecture/p02type/C02Integer.java
@@ -2,15 +2,15 @@ package c02.lecture.p02type;
 
 public class C02Integer {
     public static void main(String[] args) {
-        /* 정수타입
-            int
+            //정수타입
+//            int
+
             int a = 5;
             int b = 10;
 
-            long
+//            long
+            long c = 30_341_324L;
 
-            byte, short
-
-         */
+//            byte, short
     }
 }
