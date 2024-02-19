3547c3a6e362e696ca7cb36af9b5ddd438690261
kooreung <crisishyun@gmail.com>
2024-02-19 16:02:32 +0900

    변수와 타입

---
+++ src/c02/lecture/p02type/C02Integer.java
@@ -0,0 +1,16 @@
+package c02.lecture.p02type;
+
+public class C02Integer {
+    public static void main(String[] args) {
+        /* 정수타입
+            int
+            int a = 5;
+            int b = 10;
+
+            long
+
+            byte, short
+
+         */
+    }
+}
