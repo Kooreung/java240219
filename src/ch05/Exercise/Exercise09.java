0a529ed3002b60608e08c7e30db76069eec1cff4
kooreung <crisishyun@gmail.com>
2024-02-29 11:09:34 +0900

    240229_오전_array_unclear

--- src/ch05/Exercise/Exercise09.java
+++ src/ch05/Exercise/Exercise09.java
@@ -3,19 +3,21 @@ package ch05.Exercise;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Exercise09_unclear {
+public class Exercise09 {
     public static void main(String[] args) {
         Scanner selector = new Scanner(System.in);
         Scanner scanner = new Scanner(System.in);
+        int[][] student = {};
+        boolean start = true;
 
-        while (true) {
+        // 학생 당 5과목의 점수를 입력하는 기능을 제작하던 중
+
+        while (start) {
             System.out.println("------------------------------------------------");
             System.out.println("1.학생 수 | 2.점수입력 | 3.점수리스트 | 4.분석 | 5.종료");
             System.out.println("------------------------------------------------");
             System.out.print("선택> ");
             int select = selector.nextInt();
-            int[][] student = {};
-            int scores = 0;
 
             if (select == 1) {
                 System.out.print("학생 수> ");
@@ -34,12 +36,17 @@ public class Exercise09_unclear {
                 System.out.println("");
             }
 
-            System.out.println(Arrays.toString(student));
-            System.out.println(student.length);
-            System.out.println(Arrays.toString(student[0]));
+            // a 학생의 점수는 {n, n, n, n, n} 입니다.
+            // 로 표기하는 기능을 추가 필요
+
+            if (select == 5) {
+                start = false;
+                System.out.println("프로그램이 종료되었습니다.");
+                System.out.println("");
+                System.out.println("");
+                System.out.println("");
+            }
         }
-
-
 
     }
 }
