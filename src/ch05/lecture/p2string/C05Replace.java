55c098ae6c038e7ba2571186b7b218484b583062
kooreung <crisishyun@gmail.com>
2024-02-29 15:16:29 +0900

    240229_오후_StringReplace

---
+++ src/ch05/lecture/p2string/C05Replace.java
@@ -0,0 +1,19 @@
+package ch05.lecture.p2string;
+
+public class C05Replace {
+    public static void main(String[] args) {
+        //replace 특정 문자열을 바꾼 문자열을 리턴
+        //-> 원본 문자열이 바뀌는 건 아니다.
+
+        String s = "my name is son";
+        String t = s.replace("my", "your");
+
+        System.out.println(s);
+        System.out.println(t);
+
+        String u = "java, spring, css, html";
+        String v = u.replace("java","자바");
+        System.out.println(u);
+        System.out.println(v);
+    }
+}
