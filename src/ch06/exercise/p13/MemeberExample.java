1fffa1513ed64d2478c574a6ca53cfe5b1ac407d
kooreung <crisishyun@gmail.com>
2024-03-04 12:34:35 +0900

    240304_오전_Field3

---
+++ src/ch06/exercise/p13/MemeberExample.java
@@ -0,0 +1,13 @@
+package ch06.exercise.p13;
+
+public class MemeberExample {
+    public static void main(String[] args) {
+        Member member1 = new Member();
+        member1.name = "son";
+
+        Member member2 = new Member();
+        member2.name = "lee";
+    }
+
+
+}
