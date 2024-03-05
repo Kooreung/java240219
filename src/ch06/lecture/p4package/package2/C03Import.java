24f4fdc87fce565ff3bdc0ebf9083c72c978a771
kooreung <crisishyun@gmail.com>
2024-03-05 15:51:56 +0900

    240305_오후_ClassImport

---
+++ src/ch06/lecture/p4package/package2/C03Import.java
@@ -0,0 +1,11 @@
+package ch06.lecture.p4package.package2;
+
+import java.util.Scanner;
+import java.lang.String;
+// java.lang 패키지의 클래스는 import 없어도 사용 가능 (= 생략 가능)
+public class C03Import {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner("");
+        String str = "hello";
+    }
+}
--- src/ch06/sec12/hyundai/Car.java
+++ src/ch06/sec12/hyundai/Car.java
@@ -1,6 +1,7 @@
 package ch06.sec12.hyundai;
-import ch06.sec12.hankook.SnowTire;
+//import ch06.sec12.hankook.SnowTire;
 import ch06.sec12.kumho.AllSeasonTire;
+import ch06.sec12.hankook.*;
 
 public class Car {
     ch06.sec12.hankook.Tire tire1 = new ch06.sec12.hankook.Tire();
