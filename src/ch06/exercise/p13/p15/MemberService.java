f75108140d87e0bcf585b45b67da7ef85aeb3a86
kooreung <crisishyun@gmail.com>
2024-03-05 10:39:39 +0900

    240305_오전_Return

---
+++ src/ch06/exercise/p13/p15/MemberService.java
@@ -0,0 +1,18 @@
+package ch06.exercise.p13.p15;
+
+public class MemberService {
+
+    boolean login(String id, String password) {
+        if (id.equals("hong")) {
+            if (password.equals("12345")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void logout(String id) {
+        System.out.println(id + "님이 로그아웃 되었습니다.");
+        return;
+    }
+}
