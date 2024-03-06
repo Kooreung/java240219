f48b811fa895357e4672dbd58a5a39e6e13a9edb
kooreung <crisishyun@gmail.com>
2024-03-06 15:01:01 +0900

    240306_오전_AccessModifier

--- src/ch06/lecture/p7access/package1/C03AccessModifier.java
+++ src/ch06/lecture/p7access/package1/C03AccessModifier.java
@@ -1,9 +1,9 @@
 package ch06.lecture.p7access.package1;
 
-import ch06.lecture.p7access.C01MyClass;
+//import ch06.lecture.p7access.C01MyClass;
 
 public class C03AccessModifier {
-    public static void main(String[] args) {
-        C01MyClass obj1 = new C01MyClass();
-    }
+//    public static void main(String[] args) {
+//        C01MyClass obj1 = new C01MyClass();
+//    }
 }
