44d860720529b010ac284ecf73739bed29f341b8
kooreung <crisishyun@gmail.com>
2024-03-08 12:34:21 +0900

    240308_오전_Abstract

---
+++ src/ch07/lecture/p4abstract/C01Abstract.java
@@ -0,0 +1,17 @@
+package ch07.lecture.p4abstract;
+
+public class C01Abstract {
+    public static void main(String[] args) {
+        C01Animal a1 = new C01Tiger();
+        C01Animal b1 = new C01Cat();
+
+        // 추상 클래스로 인스턴스 생성 불가
+//        C01Animal c1 = new C01Animal(); // x
+    }
+}
+
+// @formatter:off
+// 추상 클래스 (abstract class)
+abstract class C01Animal {}
+class C01Tiger extends C01Animal {}
+class C01Cat extends C01Animal {}
