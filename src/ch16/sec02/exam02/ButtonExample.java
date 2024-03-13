dd07b6c2d23a5991d532cc43f3663fa11c97b774
kooreung <crisishyun@gmail.com>
2024-03-13 11:50:50 +0900

    240313_오전_Lambda

---
+++ src/ch16/sec02/exam02/ButtonExample.java
@@ -0,0 +1,21 @@
+package ch16.sec02.exam02;
+
+public class ButtonExample {
+    public static void main(String[] args) {
+        Button btnOk = new Button();
+
+        btnOk.setClickListener(() -> {
+            System.out.println("OK 버튼을 클릭했습니다.");
+        });
+
+        btnOk.click();
+
+        Button btnCancel = new Button();
+
+        btnCancel.setClickListener(() -> {
+            System.out.println("Cancel 버튼을 클릭했습니다.");
+        });
+
+        btnCancel.click();
+    }
+}
