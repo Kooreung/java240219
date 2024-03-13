6c45f7827fe9de03565ad78cba6a7909cb9b8235
kooreung <crisishyun@gmail.com>
2024-03-13 11:58:10 +0900

    240313_오전_Lambda

---
+++ src/ch16/sec04/LambdaExample.java
@@ -0,0 +1,21 @@
+package ch16.sec04;
+
+import static java.lang.Double.sum;
+
+public class LambdaExample {
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        person.action((x, y) -> {
+            double result = x + y;
+            return result;
+        });
+
+        person.action((x, y) -> (x + y));
+        person.action((x, y) -> sum(x,y));
+    }
+
+    public static double sum(double x, double y) {
+        return (x + y);
+    }
+}
