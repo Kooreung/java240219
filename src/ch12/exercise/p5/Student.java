66c89c0f7898c28f57ded47a3120b25b1b0631a6
kooreung <crisishyun@gmail.com>
2024-03-14 15:16:17 +0900

    240314_오후_javaBaseObjectHashCode

---
+++ src/ch12/exercise/p5/Student.java
@@ -0,0 +1,28 @@
+package ch12.exercise.p5;
+
+import java.util.Objects;
+
+public class Student {
+    private String studentNum;
+
+    public Student(String studentNum) {
+        this.studentNum = studentNum;
+    }
+
+    public String getStudentNum() {
+        return studentNum;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(studentNum);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Student student = (Student) obj;
+        return Objects.equals(studentNum, student.studentNum);
+    }
+}
