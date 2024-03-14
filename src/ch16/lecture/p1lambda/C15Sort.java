d372f9fa4b1dff51f448895648df6a60ec077a34
kooreung <crisishyun@gmail.com>
2024-03-14 10:11:44 +0900

    240314_오전_SortWorkflow

---
+++ src/ch16/lecture/p1lambda/C15Sort.java
@@ -0,0 +1,9 @@
+package ch16.lecture.p1lambda;
+
+public class C15Sort {
+    public static void main(String[] args) {
+        String[] names = {"son", "kwang", "alex", "go", "johnes"};
+        int loop = names.length;
+
+    }
+}
