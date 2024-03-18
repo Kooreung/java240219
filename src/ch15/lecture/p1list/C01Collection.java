351c105e51bb93565279bcd0bfa405658d7d18c4
kooreung <crisishyun@gmail.com>
2024-03-18 10:44:44 +0900

    240318_오전_Collection

---
+++ src/ch15/lecture/p1list/C01Collection.java
@@ -0,0 +1,15 @@
+package ch15.lecture.p1list;
+
+public class C01Collection {
+    public static void main(String[] args) {
+        // Collection FrameWork
+        // 데이터를 저장하고 관리하기 위한 구조
+
+        // 자료 구조를 구현한 클래스, 인터페이스, 메소드 등
+
+        // 자주 쓰는 자료구조
+        // List , Set , Map, Stack, Queue
+
+        // 구현된 메소드 : 데이터 추가, 검색, 삭제, 정렬, 교체
+    }
+}
