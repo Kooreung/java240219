5755a06c07bac349f1838318b739a84a21df2897
kooreung <crisishyun@gmail.com>
2024-03-18 14:00:46 +0900

    240318_오전_CollectionList

--- src/ch15/exercise/p7/BoardDao.java
+++ src/ch15/exercise/p7/BoardDao.java
@@ -12,7 +12,6 @@ public class BoardDao {
         list.add(new Board("제목1" , "내용1"));
         list.add(new Board("제목2" , "내용2"));
         list.add(new Board("제목3" , "내용3"));
-
         return list;
     }
 }
