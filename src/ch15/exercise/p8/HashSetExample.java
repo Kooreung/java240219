c041a52846580c0faa3ec3b4a990a9cc2b40cd29
kooreung <crisishyun@gmail.com>
2024-03-19 15:38:38 +0900

    240319_오후_CollectionHashSet

---
+++ src/ch15/exercise/p8/HashSetExample.java
@@ -0,0 +1,20 @@
+package ch15.exercise.p8;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetExample {
+    public static void main(String[] args) {
+        Set<Student> set = new HashSet<Student>();
+
+        set.add(new Student(1, "홍길동"));
+        set.add(new Student(2, "홍길동"));
+        set.add(new Student(1, "조민우"));
+
+        System.out.println("저장된 객체 수 : " + set.size());
+        for (Student s : set) {
+            System.out.println(s.studentNum + ":" + s.name);
+        }
+
+    }
+}
