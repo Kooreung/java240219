2507f1dcb2b0d7d5cdc4bea1973c09d1ffa81dff
kooreung <crisishyun@gmail.com>
2024-03-19 12:37:51 +0900

    240319_오전_CollectionSetEquals

---
+++ src/ch15/sec03/exam02/Member.java
@@ -0,0 +1,26 @@
+package ch15.sec03.exam02;
+
+import java.util.Objects;
+
+public class Member {
+    public String name;
+    public int age;
+
+    public Member(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return age == member.age && Objects.equals(name, member.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
