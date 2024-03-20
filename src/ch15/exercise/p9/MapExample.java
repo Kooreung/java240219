8f57311ae9e23b76efe79c7d584c6c16d3c4b6fa
kooreung <crisishyun@gmail.com>
2024-03-20 16:40:18 +0900

    240320_오후_Map

---
+++ src/ch15/exercise/p9/MapExample.java
@@ -0,0 +1,19 @@
+package ch15.exercise.p9;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExample {
+    public static void main(String[] args) {
+        Map<String,Integer> map = new HashMap<String, Integer>();
+        map.put("blue", 96);
+        map.put("hong", 86);
+        map.put("white", 92);
+
+        String name = null;
+        int maxScore = 0;
+        int totalScore = 0;
+
+
+    }
+}
