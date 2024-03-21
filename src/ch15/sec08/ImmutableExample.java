156ec49a009ff7c3e33d7c9aafc15fec77618b0f
kooreung <crisishyun@gmail.com>
2024-03-21 12:15:23 +0900

    240321_오전_UnmodifiableMethod

---
+++ src/ch15/sec08/ImmutableExample.java
@@ -0,0 +1,39 @@
+package ch15.sec08;
+
+import java.util.*;
+
+public class ImmutableExample {
+    public static void main(String[] args) {
+        List<String> immutableList1 = List.of("A", "B", "C");
+
+        Set<String> immutableSet1 = Set.of("A", "B", "C");
+
+        Map<Integer, String> immutableMap1 = Map.of(
+                1, "A",
+                2, "B",
+                3, "C"
+        );
+
+        List<String> list = new ArrayList<>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        List<String> immutableList2 = List.copyOf(list);
+
+        Set<String> set = new HashSet<>();
+        set.add("A");
+        set.add("B");
+        set.add("C");
+        Set<String> immutableset2 = Set.copyOf(set);
+
+        Map<Integer, String> map = new HashMap<>();
+        map.add(1, "A");
+        map.add(2, "B");
+        map.add(3, "C");
+        Map<Integer, String> immutablemap2 = Map.copyOf(map);
+
+        String[]arr = {"A", "B", "C"};
+        List<String> immutableList3 = Arrays.asList(arr);
+
+    }
+}
