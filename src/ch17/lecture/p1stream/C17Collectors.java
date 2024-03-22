d4bbe0627162fafd131a669fc3bc912ce76c9c3a
kooreung <crisishyun@gmail.com>
2024-03-22 16:15:51 +0900

    240322_오후_StreamCollectCollectors

---
+++ src/ch17/lecture/p1stream/C17Collectors.java
@@ -0,0 +1,17 @@
+package ch17.lecture.p1stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class C17Collectors {
+    public static void main(String[] args) {
+        List<String> list
+                = List.of("java", "spring", "son", "lee", "otani");
+
+        // Collectors.counting
+        Long collect = list.stream()
+                .collect(Collectors.counting());
+
+        System.out.println("collect = " + collect);
+    }
+}
---
+++ src/ch17/lecture/p1stream/C18Collectors.java
@@ -0,0 +1,24 @@
+package ch17.lecture.p1stream;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class C18Collectors {
+    public static void main(String[] args) {
+        List<String> list = List.of(
+                "java", "css", "html",
+                "spring", "jquery", "c++",
+                "swift", "home", "javascript"
+        );
+
+        // Map<String, Long>
+        // {"j" : 3, "c" : 2, "h" : 2, "s" :2}
+
+        Map<String, Long> collect = list.stream()
+                .collect(Collectors.groupingBy(s -> s.substring(0, 1),
+                        Collectors.counting()));
+
+        System.out.println("collect = " + collect);
+    }
+}
