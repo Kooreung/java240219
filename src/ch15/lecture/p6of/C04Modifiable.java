c386543b6070855cc1e91ae9fa3dd4c06ebdcb61
kooreung <crisishyun@gmail.com>
2024-03-22 09:21:12 +0900

    240321_오전_UnmodifiableMethod

--- src/ch15/lecture/p6of/C04Modifiable.java
+++ src/ch15/lecture/p6of/C04Modifiable.java
@@ -6,8 +6,8 @@ import java.util.List;
 public class C04Modifiable {
     public static void main(String[] args) {
         List<Integer> list = List.of(4, 1, 0, 9);
+        // 수정 가능하도록 변형
         List<Integer> list2 = new ArrayList<>(list);
         list2.add(10);
-        // 수정 가능하도록 변형
     }
 }
