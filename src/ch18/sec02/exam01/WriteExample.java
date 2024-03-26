684f72cbbb641dc572ff6ffe2b444b9daab4019e
kooreung <crisishyun@gmail.com>
2024-03-26 17:20:25 +0900

    240326_오후_FileOutputStream

---
+++ src/ch18/sec02/exam01/WriteExample.java
@@ -0,0 +1,29 @@
+package ch18.sec02.exam01;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class WriteExample {
+    public static void main(String[] args) throws IOException {
+        try {
+            OutputStream os = new FileOutputStream("temp/test1.db");
+
+            byte a = 10;
+            byte b = 20;
+            byte c = 30;
+
+            os.write(a);
+            os.write(b);
+            os.write(c);
+
+            os.flush();
+            os.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
